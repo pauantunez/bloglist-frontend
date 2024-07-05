2a77b0071eb1d457ff34f2b4cbae8fffde8284bf
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-07-05 10:09:43 +0200

    exercice 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    author: "",
+    url: "",
+  });
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
@@ -49,9 +55,34 @@ const App = () => {
     blogService.setToken(null);
   };
 
+  const handleBlogCreation = async (event) => {
+    event.preventDefault();
+
+    try {
+      blogService.create(newBlog).then((returnedBlog) => {
+        setBlogs(blogs.concat(returnedBlog));
+        setNewBlog({ title: "", author: "", url: "" });
+      });
+    } catch (exception) {
+      setErrorMessage("Error creating blog");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const handleBlogChange = ({ target }) => {
+    const { name, value } = target;
+    setNewBlog({
+      ...newBlog,
+      [name]: value,
+    });
+  };
+
   const loginForm = () => (
     <div>
       <h2>log in to application</h2>
+      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
       <form onSubmit={handleLogin}>
         <div>
           username
@@ -66,12 +97,34 @@ const App = () => {
     </div>
   );
 
+  const blogForm = () => (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleBlogCreation}>
+        <div>
+          title
+          <input type="text" value={newBlog.title} name="title" onChange={handleBlogChange} />
+        </div>
+        <div>
+          author
+          <input type="text" value={newBlog.author} name="author" onChange={handleBlogChange} />
+        </div>
+        <div>
+          url
+          <input type="text" value={newBlog.url} name="url" onChange={handleBlogChange} />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+
   const blogList = () => (
     <div>
       <h2>blogs</h2>
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
+      {blogForm()}
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
