c6aab3aa026c426980ec1af773e635defbf49689
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-07-05 09:46:27 +0200

    exercice 5.2

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,12 @@ const App = () => {
     }
   };
 
+  const handleLogout = () => {
+    window.localStorage.removeItem("loggedBlogappUser");
+    setUser(null);
+    blogService.setToken(null);
+  };
+
   const loginForm = () => (
     <div>
       <h2>log in to application</h2>
@@ -60,20 +66,19 @@ const App = () => {
     </div>
   );
 
-  return (
+  const blogList = () => (
     <div>
-      {!user && loginForm()}
-      {user && (
-        <div>
-          <h2>blogs</h2>
-          <p>{user.name} logged in</p>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
-        </div>
-      )}
+      <h2>blogs</h2>
+      <p>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+      </p>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
     </div>
   );
+
+  return <div>{!user ? loginForm() : blogList()}</div>;
 };
 
 export default App;
