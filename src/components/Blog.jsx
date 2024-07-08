62700cf7ec85d53d3a19b0e377f86ed3963c6f75
Pau A. (Novaigrup) <pau.antunez@novaigrup.com>
2024-07-08 11:58:29 +0200

    exercice 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} of {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleToggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <strong>{blog.title}</strong> by {blog.author} <button onClick={handleToggleDetails}>{showDetails ? "hide" : "view"}</button>
+      </div>
+      {showDetails && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes} <button onClick={() => console.log("like button clicked")}>like</button>
+          </p>
+          <p>added by {blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
